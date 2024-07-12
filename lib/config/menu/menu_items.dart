1958638ee1d748639690c9a89e2f5bb88bd42a0a
Oscar Coronel <oscar.coronel@conauto.com.ec>
2024-07-12 11:20:51 -0500

    Agregamos menuItems

---
+++ lib/config/menu/menu_items.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+
+class MenuItems {
+  
+  final String title;
+  final String subtitle;
+  final String link;
+  final IconData icon;
+
+  const MenuItems({
+    required this.title,
+    required this.subtitle,
+    required this.link,
+    required this.icon
+  });
+
+}
+
+const appMenuItems = <MenuItems>[
+  MenuItems(
+    title: 'Botones',
+    subtitle: 'Varios botones en Flutter',
+    link: '/buttons',
+    icon: Icons.smart_button_outlined
+  ),
+  MenuItems(
+    title: 'Tarjetas',
+    subtitle: 'Un contenedor estilizado',
+    link: '/card',
+    icon: Icons.credit_card
+  ),
+];
