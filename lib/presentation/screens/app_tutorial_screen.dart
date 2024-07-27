f71f020ccd7ea4f64be6f3dd29fe50bf3771f46f
Oscar Coronel <ocoronel31@gmail.com>
2024-07-27 14:21:39 -0500

    Arreglar el cargado de las imagenes en el bundle de mi aplicacion y finalizar app tutorial

--- lib/presentation/screens/app_tutorial_screen.dart
+++ lib/presentation/screens/app_tutorial_screen.dart
@@ -1,4 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class SlideInfo {
+  final String title;
+  final String caption;
+  final String imageUrl;
+  SlideInfo({required this.title, required this.caption, required this.imageUrl});
+}
+
+final slides = <SlideInfo>[
+  SlideInfo(title: 'Busca la comida', caption: 'Magna dolor deserunt officia adipisicing amet.', imageUrl: 'assets/images/1.png'),
+  SlideInfo(title: 'Entrega rápida', caption: 'Et culpa ipsum adipisicing velit excepteur ad.', imageUrl: 'assets/images/2.png'),
+  SlideInfo(title: 'Disfruta la comida', caption: 'Aute ad ullamco exercitation adipisicing ullamco.', imageUrl: 'assets/images/3.png'),
+];
+
 
 class AppTutorialScreen extends StatelessWidget {
 
@@ -8,11 +23,9 @@ class AppTutorialScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('App Tutorial'),
-      ),
-      body: const _AppTutorialView(),
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: _AppTutorialView(),
     );
   }
 
@@ -24,7 +37,66 @@ class _AppTutorialView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Stack(
+      children: [
+        PageView(
+          physics: const BouncingScrollPhysics(),
+          children: slides.map((slide){
+            return _Slide(title: slide.title, caption: slide.caption, imageUrl: slide.imageUrl);
+          }).toList(),
+        ),
+
+        Positioned(
+          right: 20,
+          top: 50,
+          child: TextButton(
+            onPressed: (){
+              context.pop();
+            },
+            child: const Text('Salir')
+          )
+        )
+      ],
+    );
   }
-  
+
+}
+
+class _Slide extends StatelessWidget {
+
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  const _Slide({
+    required this.title,
+    required this.caption,
+    required this.imageUrl
+  });
+
+  @override
+  Widget build(BuildContext context) {
+
+    final titleStyle = Theme.of(context).textTheme.titleLarge;
+    final captionStyle = Theme.of(context).textTheme.bodySmall;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric( horizontal: 30 ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image(
+              image: AssetImage(imageUrl)
+            ),
+            const SizedBox(height: 20,),
+            Text(title, style: titleStyle),
+            const SizedBox(height: 10,),
+            Text(caption, style: captionStyle)
+          ],
+        ),
+      ),
+    );
+  }
 }
